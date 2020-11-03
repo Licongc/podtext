8397d7ebfc5324ad72c93ccaf284c58a6bacf389
licc <congcong.li@lvyuetravel.com>
2020-11-03 18:22:10 +0800

    ddas

--- Pods/Target Support Files/Pods-podtest2-podtest2UITests/Pods-podtest2-podtest2UITests-frameworks.sh
+++ Pods/Target Support Files/Pods-podtest2-podtest2UITests/Pods-podtest2-podtest2UITests-frameworks.sh
@@ -175,9 +175,13 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/ReactiveObjC/ReactiveObjC.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/localpod2/localpod2.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/ReactiveObjC/ReactiveObjC.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/localpod2/localpod2.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
--- libs/lib/localpod2.podspec
+++ libs/lib/localpod2.podspec
@@ -133,6 +133,6 @@ Pod::Spec.new do |spec|
 
   # spec.xcconfig = { "HEADER_SEARCH_PATHS" => "$(SDKROOT)/usr/include/libxml2" }
   # spec.dependency "JSONKit", "~> 1.4"
-#  spec.dependency 'AFNetworking', '~>3.0'
-#  spec.dependency 'ReactiveObjC', '~> 3.0'
+  spec.dependency 'AFNetworking', '~>3.0'
+  spec.dependency 'ReactiveObjC', '~> 3.0'
 end
